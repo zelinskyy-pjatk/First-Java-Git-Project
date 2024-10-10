660905e90eaec1a77362cc1f7be294370ecd6d22
s31639 <s31639@pjwstk.edu.pl>
2024-10-10 16:05:16 +0200

    First Java Project Commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public
+    class Main{
+    public static void main(String[] args) {
+        Adder adder = new Adder();
+        System.out.println(adder.add(1,2));
+
+        Subtractor subtractor = new Subtractor();
+        System.out.println(subtractor.subtract(6,3));
+
+    }
+}
