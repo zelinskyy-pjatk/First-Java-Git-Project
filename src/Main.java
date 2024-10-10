853dd885beafdfe3291f7a93f82326c950ba0c2f
s31639 <s31639@pjwstk.edu.pl>
2024-10-10 16:18:45 +0200

    Specified who adds Adder class and Subtractor class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 //TODO: we need to add 2 more additional classes, to get rid of errors\
 
-//OK, I will add "Adder" class and s31519 will add "Subtractor" class
+//OK, I will add "Adder" class and s31519 will add "Subtractor" class ^%^
 
 public
     class Main{
